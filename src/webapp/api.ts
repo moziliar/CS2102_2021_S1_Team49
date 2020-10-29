8e10f882017968d3f2e60a999884fbfa1f63ebe1
jonchan51 <32188705+jonchan51@users.noreply.github.com>
2020-10-29 08:31:46 +0800

    Use heroku for prod env

--- src/webapp/api.ts
+++ src/webapp/api.ts
@@ -1,9 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 const backend_url = () => {
-  // return 'https://backend-2102.herokuapp.com';
-  return 'http://127.0.0.1:8080';
-}
+  if (process.env.NODE_ENV === "production") {
+    return "https://backend-2102.herokuapp.com";
+  }
+
+  // local testing
+  return "http://127.0.0.1:8080";
+};
 
 export default axios.create({
   baseURL: backend_url(),
